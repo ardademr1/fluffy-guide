ae4b6b9284e25e3f264089283bc42d4075bacb15
Arda Demir <ardaofis1@outlook.com>
2019-07-10 16:44:44 +0300

    teslim

--- app/src/main/java/com/example/fluffy_guide/Main2Activity.java
+++ app/src/main/java/com/example/fluffy_guide/Main2Activity.java
@@ -3,6 +3,7 @@ package com.example.fluffy_guide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        String adSoyad = getIntent().getStringExtra("AdSoyad");
+        TextView textView2;
+        textView2 = findViewById(R.id.textView2);
+        textView2.setText("hoşgeldin " + adSoyad);
     }
 }
--- app/src/main/java/com/example/fluffy_guide/MainActivity.java
+++ app/src/main/java/com/example/fluffy_guide/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.fluffy_guide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
+    EditText editText2;
+    EditText editTextPassword1;
+    EditText editTextPassword2;
+    EditText editText5;
+    ImageView imageView2;
+    Button button;
+    Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        textView = findViewById(R.id.textView);
+        editText2 = findViewById(R.id.editText2);
+        editTextPassword1 = findViewById(R.id.editTextPassword1);
+        editTextPassword2 = findViewById(R.id.editTextPassword2);
+        editText5 = findViewById(R.id.editText5);
+        imageView2 = findViewById(R.id.imageView2);
+        button = findViewById(R.id.button3);
+        button1 = findViewById(R.id.button4);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText2.setText("Jonh Marston");
+            }
+        });
+            button1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(editTextPassword1.getText().toString().equals(editTextPassword2.getText().toString())) {
+                        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                        intent.putExtra("AdSoyad", editText2.getText().toString());
+                        startActivity(intent);
+                    }
+                }
+            });
+        }
 }
